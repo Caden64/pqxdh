390a2128a2e06421120241de0c8425995718bcf1
caden32 <caden@caden32.com>
2023-09-25 17:16:28 -0600

    prekey bundle better implemented

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,55 @@
 use ed25519_compact::{KeyPair, Noise, Signature};
-use ed25519_compact::PublicKey;
+use ed25519_compact::{PublicKey, SecretKey};
 use ed25519_compact::x25519::KeyPair as xK;
-use pqc_kyber::{keypair, Keypair};
+use pqc_kyber::{keypair, Keypair, KyberError};
+use pqc_kyber::PublicKey as pqcPublicKey;
 
 fn main() {
-    println!("{:?}", PrekeyBundle::new())
+    println!("{:?}", PreKeyBundle::new().unwrap())
 }
 
 #[derive(Debug)]
-struct PrekeyBundle {
+struct PreKeyBundle {
     ik: PublicKey,
     spk: ed25519_compact::x25519::PublicKey,
     opk: ed25519_compact::x25519::PublicKey,
     spk_sig: Signature,
     opk_sig: Signature,
-    pqkem: Keypair,
+    pqkem: pqcPublicKey,
     pqkem_sig: Signature
 }
 
-impl PrekeyBundle {
-    fn new() -> PrekeyBundle {
+#[derive(Debug)]
+struct PrivateKeyBundle {
+    ik: SecretKey,
+    spk: ed25519_compact::x25519::SecretKey,
+    opk: ed25519_compact::x25519::SecretKey,
+    pqkem: Keypair
+}
+
+impl PreKeyBundle {
+    fn new() -> Result<(PreKeyBundle, PrivateKeyBundle), KyberError> {
         let bob_ik = KeyPair::generate();
         let bob_spk = xK::generate();
         let bob_opk = xK::generate();
         let spk_sig = bob_ik.sk.sign(bob_spk.pk.as_ref(), Some(Noise::default()));
         let opk_sig = bob_ik.sk.sign(bob_opk.pk.as_ref(), Some(Noise::default()));
         let mut rng = rand::thread_rng();
-        let bob_pqkem = keypair(&mut rng).unwrap();
+        let bob_pqkem = keypair(&mut rng)?;
         let pqkem_sig = bob_ik.sk.sign(bob_pqkem.public.as_ref(), Some(Noise::default()));
-        PrekeyBundle{ ik: bob_ik.pk, spk: bob_spk.pk, opk: bob_opk.pk, spk_sig, opk_sig, pqkem: bob_pqkem, pqkem_sig }
+        Ok((PreKeyBundle {
+            ik: bob_ik.pk,
+            spk: bob_spk.pk,
+            opk: bob_opk.pk,
+            spk_sig: spk_sig,
+            opk_sig: opk_sig,
+            pqkem: bob_pqkem.public,
+            pqkem_sig
+        }, PrivateKeyBundle {
+            ik: bob_ik.sk,
+            spk: bob_spk.sk,
+            opk: bob_opk.sk,
+            pqkem: bob_pqkem,
+        }))
     }
 }
