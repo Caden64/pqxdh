72ca98ed8b03f95d5a1b95c8d713ca827e564bb9
caden32 <caden@caden32.com>
2023-09-30 20:47:59 -0600

    initial message partially implemented

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use argon2::Version;
+use sha2::Sha256;
+use hkdf::Hkdf;
 use ed25519_compact::{KeyPair, Noise, Signature};
 use ed25519_compact::{PublicKey, SecretKey};
 use ed25519_compact::x25519::KeyPair as xK;
-use pqc_kyber::{keypair, Keypair, KyberError};
+use pqc_kyber::{keypair, Keypair, KyberError, encapsulate};
 use pqc_kyber::PublicKey as pqcPublicKey;
 
 fn main() {
     let pkb = PreKeyBundle::new().unwrap();
-    println!("{:?}", pkb.0.ik)
+    InitialMessage::alice_handle_pre_key(pkb.0);
 }
 
 #[derive(Debug)]
@@ -43,8 +44,8 @@ impl PreKeyBundle {
             ik: bob_ik.pk,
             spk: bob_spk.pk,
             opk: bob_opk.pk,
-            spk_sig: spk_sig,
-            opk_sig: opk_sig,
+            spk_sig,
+            opk_sig,
             pqkem: bob_pqkem.public,
             pqkem_sig
         }, PrivateKeyBundle {
@@ -62,4 +63,29 @@ struct InitialMessage<'a> {
     ed: ed25519_compact::x25519::PublicKey,
     ct: pqc_kyber::Encapsulated,
     ect: argon2::Argon2<'a>,
+}
+
+impl<'a> InitialMessage<'a> {
+    pub fn alice_handle_pre_key(pkb: PreKeyBundle) {
+        if let Err(e) = pkb.ik.verify(pkb.spk.as_ref(), &pkb.spk_sig) {
+            panic!("Error: {}", e)
+        }
+        if let Err(e) = pkb.ik.verify(pkb.opk.as_ref(), &pkb.opk_sig) {
+            panic!("Error: {}", e)
+        }
+        if let Err(e) = pkb.ik.verify(pkb.pqkem.as_ref(), &pkb.pqkem_sig) {
+            panic!("Error: {}", e)
+        }
+        let mut rng = rand::thread_rng();
+        let (ct, ss) = encapsulate(&pkb.pqkem, &mut rng).unwrap();
+        let bob_ik_x25519 = ed25519_compact::x25519::PublicKey::from_ed25519(&pkb.ik).unwrap();
+        let alice_ik = xK::generate();
+        let alice_ek = xK::generate();
+        let dh1 = pkb.spk.dh(&alice_ik.sk).unwrap();
+        let dh2 = bob_ik_x25519.dh(&alice_ek.sk).unwrap();
+        let dh3 = pkb.spk.dh(&alice_ek.sk).unwrap();
+        let dh4 = pkb.opk.dh(&alice_ek.sk).unwrap();
+        let sum = [dh1.as_ref(), dh2.as_ref(), dh3.as_ref(), dh4.as_ref(), ss.as_slice()].concat();
+        let hk = Hkdf::<Sha256>::new(None, sum.as_slice());
+    }
 }
